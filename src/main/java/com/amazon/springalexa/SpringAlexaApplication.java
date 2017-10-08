359db800888c6c151e67798c5093faf0dff9526b
Thiru <rthirucs@gmail.com>
2017-10-08 15:47:42 +0530

    Initial commit

---
+++ src/main/java/com/amazon/springalexa/SpringAlexaApplication.java
@@ -0,0 +1,12 @@
+package com.amazon.springalexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlexaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/amazon/springalexa/SpringAlexaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amazon.springalexa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAlexaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
