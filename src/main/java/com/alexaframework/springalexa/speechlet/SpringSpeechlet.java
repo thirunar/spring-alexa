ac92cbc4f0a5a6f7f691d603044994cc117f6fcb
Thiru <rthirucs@gmail.com>
2017-10-09 23:58:52 +0530

    #2 | Thiru | responding with static value when handler is not present

--- src/main/java/com/alexaframework/springalexa/speechlet/SpringSpeechlet.java
+++ src/main/java/com/alexaframework/springalexa/speechlet/SpringSpeechlet.java
@@ -1,32 +1,43 @@
 package com.alexaframework.springalexa.speechlet;
 
 import com.alexaframework.springalexa.intent.IntentHandler;
+import com.alexaframework.springalexa.intent.StaticIntentHandler;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public abstract class SpringSpeechlet implements Speechlet {
 
+    private final StaticIntentHandler staticIntentHandler;
     private List<IntentHandler> intentHandlers;
 
     @Autowired
-    public SpringSpeechlet(IntentHandler... intentHandlers) {
+    public SpringSpeechlet(StaticIntentHandler staticIntentHandler,
+                           IntentHandler... intentHandlers) {
+        this.staticIntentHandler = staticIntentHandler;
         this.intentHandlers = Arrays.asList(intentHandlers);
     }
 
     @Override
     public SpeechletResponse onIntent(IntentRequest intentRequest,
                                       Session session) throws SpeechletException {
-        Intent intent = intentRequest.getIntent();
-        for (IntentHandler handler : intentHandlers) {
-            if (handler.canHandle(intent.getName())) {
-                return handler.handle(session, intent);
+        try {
+            Intent intent = intentRequest.getIntent();
+
+            for (IntentHandler handler : intentHandlers) {
+                if (handler.canHandle(intent.getName())) {
+                    return handler.handle(session, intent);
+                }
             }
+            return staticIntentHandler.handle(session, intent);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
